2cab7008435440b12e611f1f2c2563949c675231
Leonardo Gomes <55643297+JoltLeo@users.noreply.github.com>
2021-12-27 02:01:35 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ terraform {
     tenant_id            = "bd41b059-b038-488a-bdb8-e0d259819fc5"
     subscription_id      = "21598ca8-3b69-42da-98d7-eb0867540218"
     client_id            = "6749c383-3df2-4028-a974-7711d176cd67"
-    client_secret        = #{APP_SECRET}#
+    client_secret        = "#{APP_SECRET}#"
   }
 
   required_providers {
