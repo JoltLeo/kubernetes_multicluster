43af559303b3fb18b7277416b8ea026680d77ff1
JoltLeo <leo.gcs@poli.ufrj.br>
2022-03-21 14:56:40 -0300

    remove wrong output

--- terraform/modules/aws_kubernetes/outputs.tf
+++ terraform/modules/aws_kubernetes/outputs.tf
@@ -18,11 +18,6 @@ output "kube_config" {
   value       = local.kubeconfig
 }
   
-output "config_map_aws_auth" {
-  description = "A kubernetes configuration to authenticate to this EKS cluster."
-  value       = module.eks.config_map_aws_auth
-}
-
 output "region" {
   description = "AWS region"
   value       = var.clusters_region
